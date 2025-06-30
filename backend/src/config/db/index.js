ffa8a39fc8c01d53c4d6fd958a4b6add6e544cfb
Nglg16 <luongndhe172441@fpt.edu.vn>
2025-06-30 20:10:25 +0700

    demoback

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -1,18 +1,19 @@
-
 const mongoose = require("mongoose");
+const config = require("../../../config/env");
+const logger = require("../../utils/logger");
 
-async function connectDB() {
+async function connect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("✅ Đã kết nối MongoDB Atlas với Mongoose");
-
-    const db = mongoose.connection;
-
-    return db;
-  } catch (err) {
-    console.error("❌ Lỗi kết nối MongoDB:", err.message);
-    throw err;
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info("Connected to MongoDB");
+  } catch (error) {
+    logger.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Exit application if connection fails
   }
 }
 
-module.exports = { connectDB };
+module.exports = { connect };
